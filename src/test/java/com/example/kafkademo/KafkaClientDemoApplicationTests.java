aa3a35311cc1bac5065e854f77feb60cefce1a61
Tim Lynch <lynch0001@gmail.com>
2024-03-24 04:56:05 -0700

    test fix

---
+++ src/test/java/com/example/kafkademo/KafkaClientDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.kafkademo;
+
+import org.springframework.boot.SpringApplication;
+
+
+class KafkaClientDemoApplicationTests {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(KafkaClientDemoApplicationTests.class, args);
+    }
+}
